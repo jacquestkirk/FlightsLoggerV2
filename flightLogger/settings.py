399a7bf27f0c646a7b7d4825d81237a4a2e3fd76
jacquestkirk <jacquestkirk@gmail.com>
2017-01-01 02:26:44 -0600

    fix typo

--- flightLogger/settings.py
+++ flightLogger/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'logger.apps.LoggerConfig'
-])
+)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
