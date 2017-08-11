fcc88615a0570cb979485a7718e302f54d22ab07
jacquestkirk <jacquestkirk@gmail.com>
2017-08-10 19:54:06 -0500

    Add starter code for generating lists

---
+++ ListGenerators.py
@@ -0,0 +1,19 @@
+
+class ListGenerators:
+
+    def __init__(self):
+        self.airportList = []
+        self.flightDateList = []
+
+    def GenerateAirportList(self):
+        #Betty: populate this with code for reading csv file of airports
+        return
+    def GenerateFlightDateList(self):
+        #Betty: populate this with code for finding weekends based on today's date
+        return
+
+    def GetAirportList(self):
+        return self.airportList
+
+    def GetFlightDateList(self):
+        return self.flightDateList
